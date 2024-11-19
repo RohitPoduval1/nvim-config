3c083ab83b319d9b3ca45b8df5c0b3ee84f85f4f
Rohit Poduval <rohit.poduval2@gmail.com>
2024-11-19 13:14:05 -0600

    Fix visual glitch with autocmd

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 require("rohit.core")
 require("rohit.lazy")
 
+vim.cmd([[autocmd VimEnter * ++nested lua require('right-mode').apply_theme()]])
+
 vim.opt.tabstop = 4 -- A tab character is 4 spaces
 vim.opt.shiftwidth = 4 -- Indentation level is 4 spaces
 vim.opt.expandtab = true -- Use spaces instead of tabs
