9877c7e71865f35c24ee43fea0b27e7f26c09b4f
Rohit Poduval <rohit.poduval2@gmail.com>
2024-11-30 18:37:07 -0600

    Change right-mode start time

--- lua/rohit/plugins/right-mode.lua
+++ lua/rohit/plugins/right-mode.lua
@@ -5,7 +5,7 @@ return {
 			light_themes = { "catppuccin-latte" },
 			dark_themes = { "kanagawa-dragon", "tokyonight-storm", "tokyonight-night" },
 			day_start = 8, -- Light theme starting from 8 AM
-			night_start = 19, -- Dark theme starting from 7 PM
+			night_start = 18, -- Dark theme starting from 6 PM
 		})
 	end,
 }
