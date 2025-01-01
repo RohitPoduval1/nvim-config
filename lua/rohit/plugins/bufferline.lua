9c5c61caa4db0e90b14212b2374805031b92e259
Rohit Poduval <rohit.poduval2@gmail.com>
2025-01-01 16:06:26 -0600

    Clean up

--- lua/rohit/plugins/bufferline.lua
+++ lua/rohit/plugins/bufferline.lua
@@ -1,3 +1,4 @@
+-- A fancy top bar to show multiple files
 return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
--- lua/rohit/plugins/right-mode.lua
+++ lua/rohit/plugins/right-mode.lua
@@ -2,7 +2,7 @@ return {
 	"rohitpoduval1/right-mode.nvim",
 	config = function()
 		require("right-mode").setup({
-			light_themes = { "catppuccin-latte" },
+			light_themes = { "tokyonight", "catppuccin-latte"},
 			dark_themes = { "kanagawa-dragon", "tokyonight-storm", "tokyonight-night" },
 			day_start = 8, -- Light theme starting from 8 AM
 			night_start = 18, -- Dark theme starting from 6 PM
