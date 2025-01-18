b19e0dadf368411463cebc2e4e133dd1ab587fa3
Rohit Poduval <rohit.poduval2@gmail.com>
2025-01-17 19:35:29 -0600

    Add OCaml LSP

--- init.lua
+++ init.lua
@@ -3,10 +3,16 @@ require("rohit.lazy")
 
 vim.cmd([[autocmd VimEnter * ++nested lua require('right-mode').apply_theme()]])
 
-vim.opt.tabstop = 4 -- A tab character is 4 spaces
-vim.opt.shiftwidth = 4 -- Indentation level is 4 spaces
-vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.tabstop = 4       -- A tab character is 4 spaces
+vim.opt.shiftwidth = 4    -- Indentation level is 4 spaces
+vim.opt.expandtab = true  -- Use spaces instead of tabs
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+
+vim.opt.foldmethod = "indent"
+vim.o.foldenable = false
+
+vim.g.python3_host_prog = '/Users/rohitpoduval/Desktop/Programming_Projects/Receipt/receipt_env/bin/python'
